b25eff61be99b7be9b2bed7b2d43ecac5f5aaedd
Serhii Tytar <serhii.tytar@gmail.com>
2017-09-04 10:58:55 +0300

    change functions

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php
-require 'database.php';
 
 function get_all_posts() {
     global $mysqli;
@@ -13,12 +12,43 @@ function get_all_posts() {
     return $posts;
 }
 
+function get_post($id) {
+    global $mysqli;
+
+    $sql = 'SELECT title, content, image FROM posts WHERE id = ' . $id;
+    $query = mysqli_query($mysqli, $sql);
+    $post = mysqli_fetch_assoc($query);
+    return $post;
+}
+
+function delete_post($id) {
+    global $mysqli;
+
+    delete_image($id);
+    $sql = 'DELETE FROM posts WHERE id = ' . $id;
+    mysqli_query($mysqli, $sql) or die('Error ' . $mysqli->error);
+    header('Location: http://crud');
+}
+
+function delete_image($id) {
+    if(file_exists(get_name_image($id))) {
+        unlink(get_name_image($id));
+    }
+}
+
+function get_name_image($id) {
+    if(get_post($id)['image']) {
+        $name_image = 'uploads/' . get_post($id)['image'];
+    return $name_image;
+    }
+}
+
 function insert_post($title, $content, $image) {
     global $mysqli;
 
     $image_name = upload_image($image);
-    $sql = "INSERT INTO posts (title, content, image) VALUE ('$title','$content','$image_name')";
-    mysqli_query($mysqli, $sql) or die('error' . $mysqli->error);
+    $sql = "INSERT INTO posts(title, content, image) VALUE ('$title', '$content', '$image_name')";
+    mysqli_query($mysqli, $sql) or die ('Error ' . $mysqli->error);
 }
 
 function upload_image($image) {
@@ -30,9 +60,9 @@ function upload_image($image) {
 }
 
 function generate_name($image) {
-    $file_name = uniqid();
+    $name = uniqid();
     $extension = explode('.', $image['name']);
-    $image_name = $file_name . '.' . $extension[1];
+    $image_name = $name . '.' . $extension[1];
     return $image_name;
 }
 
@@ -40,8 +70,8 @@ function edit_post($id, $title, $content, $image) {
     global $mysqli;
 
     $image_name = change_image($id, $image);
-    $sql = "UPDATE posts SET title='" . $title . "', content='" . $content . "', image='" . $image_name . "' WHERE id=" . $id;
-    mysqli_query($mysqli, $sql) or die('error' . $mysqli->error);
+    $sql = "UPDATE posts SET title = '$title', content= '$content', image = '$image_name' WHERE id = $id";
+    mysqli_query($mysqli, $sql) or die('Error ' . $mysqli->error);
 }
 
 function change_image($id, $image) {
@@ -53,40 +83,4 @@ function change_image($id, $image) {
     else {
         delete_image($id);
     }
-}
-
-function delete_post($id) {
-    global $mysqli;
-
-    delete_image($id);
-    $sqlDel = 'DELETE FROM posts WHERE id=' . $id;
-    mysqli_query($mysqli, $sqlDel) or die ('error ' . $mysqli->error);
-    header('Location: http://crudlast');
-}
-
-function delete_image($id) {
-    if(get_name_image($id)) {
-        $image_name = "uploads/" . get_name_image($id);
-        if(file_exists($image_name)) {
-            unlink($image_name);
-        }
-    }
-}
-
-function get_name_image($id) {
-    global $mysqli;
-
-    $sql = 'SELECT image from posts WHERE id=' . $id;
-    $query = mysqli_query($mysqli,$sql) or die ('error ' . $mysqli->error);
-    $image_name = mysqli_fetch_assoc($query);
-    return $image_name['image'];
-}
-
-function get_post($id) {
-    global $mysqli;
-
-    $sql = 'SELECT title, content, image FROM posts WHERE id=' . $id;
-    $query = mysqli_query($mysqli, $sql);
-    $post = mysqli_fetch_assoc($query);
-    return $post;
 }
